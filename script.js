cf3655748a48c82ca3df7ecf690214609bb2735a
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-02-28 20:38:13 -0800

    problem 1 complete

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 // Basic sorting algorithms 
-    // 
+
+
+//Comparison sort 
+    // compares two items and decides which one to put first 
+
+
+
+
+
+
+/*-----------------------------------------------------------------
+Challenge: 20-isPrime
+Difficulty: Intermediate
+Prompt:
+- Write a function named isPrime that returns true when the integer argument passed to it is a prime number and false when the argument passed to it is not prime.
+- A prime number is a whole number (integer) greater than 1 that is evenly divisible by only itself.
+Examples:
+isPrime(2) //=> true
+isPrime(3) //=> true 
+isPrime(4) //=> false
+isPrime(29) //=> true
+isPrime(200) //=> false
+-----------------------------------------------------------------*/
+// Your solution for 20-isPrime here:
+
+function isPrime(n) {
+    if (n === 1) {
+      return false;
+    } else if (n === 2) {
+      return true;
+    } else {
+      for (let x = 2; x < n; x++) {
+        if (n % x === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+
+  
